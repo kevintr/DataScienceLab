86c0490bfd4b9b20729bfd5500d0441fe221643c
Kevin Tranchina <kevin.tranchina@nttdata.com>
2020-08-27 14:56:35 +0200

    praparing data con aggiunta del ts

--- kevin.py
+++ kevin.py
@@ -32,9 +32,12 @@ from matplotlib import pyplot
 from numpy import where
 import numpy
 from imblearn.under_sampling import NearMiss
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
-
+#import matplotlib.pyplot as plt
+#from matplotlib.widgets import Slider
+#import warnings
+#warnings.filterwarnings("ignore")
+#from ts_utils import *
+#install ts_utils
 # Training set upload
 training = pd.read_csv('training.csv', sep=';')    
 # verifica valori null all'interno del training
@@ -44,9 +47,35 @@ len(training['KIT_ID'].unique())
 counter = Counter(training['KIT_ID'])
 print(counter)
 
+
+import datetime
+
+
+
+def unix_time_millis(dt):
+    return (dt - epoch).total_seconds() * 1000.0
+
+training['TS'] = unix_time_millis(training['TS'])
+training['TS'] = training['TS'] - epoch
+training['TS'] = training['TS'].dt.total_seconds()
+
+
+#df_time = pd.to_datetime(training['TS'])
+#
+# 
+#
+#second= (df_time.dt.hour*60+df_time.dt.minute)*60 + df_time.dt.second
+#
+# 
+#
+#second.head()
+#second.tail()
 #Trasformazione TS in datetime
 training['TS'] = pd.to_datetime(training['TS'])
-
+training['TS'] = training['TS'].dt.total_seconds()
+import datetime, time
+t = datetime.datetime(2011, 10, 21, 0, 0)
+time.mktime(t.timetuple())
 #training['TS'] = pd.to_numeric(training['TS'], downcast='float', errors='ignore')
 
 # type(training.loc[0,'TS'] )
@@ -80,9 +109,13 @@ descriptiveQuantity
 
 
 def prepareTraining():
-    training = pd.read_csv('training.csv', sep=';')    
+    training = pd.read_csv('training.csv', sep=';')   
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    training['TS'] = pd.to_datetime(training['TS'])
+    training['TS'] = training['TS'] - epoch
+    training['TS'] = training['TS'].dt.total_seconds()
     #da inserire il TS
-    X = training[['USAGE','KIT_ID','AVG_SPEED_DW','NUM_CLI']]
+    X = training[['TS','USAGE','NUM_CLI']]
     y = training['VAR_CLASS']
     
     X = X.to_numpy()
@@ -99,6 +132,7 @@ oversample = SMOTE(random_state=100,k_neighbors=2)
 X, y = oversample.fit_resample(X, y)
 counter = Counter(y)
 print(counter)
+plot(counter)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
@@ -106,6 +140,8 @@ modelLR = LogisticRegression()
 modelLR.fit(X_train, y_train)
 y_pred = modelLR.predict(X_test)
 accuracy_score(y_test, y_pred)#0.43
+counter = Counter(y_pred)
+print(counter)
 
 #recall_score(y_test, y_pred)
 
@@ -207,7 +243,10 @@ print(counter)
 ##########################################
 model = SVC(decision_function_shape='ovo')
 # fit model
-model.fit(X_train, y_train)
+model = model.fit(X_train, y_train)
+model = model.fit(X_train, y_train)
+model = model.fit(X_train, y_train)
+model
 # make predictions
 y_pred = model.predict(x_test)
 
@@ -229,10 +268,13 @@ accuracy_score(y_test, y_pred)
 #Perceptron
 #As such, the implementation of these algorithms in the scikit-learn library implements the OvR strategy 
 #by default when using these algorithms for multi-class classification.
+training['KIT_ID'][training['VAR_CLASS'] != 2]
+training.where(training['KIT_ID'] ==3409364152 or training['KIT_ID']==1629361016 or training['KIT_ID']==2487219358)
 
-
-
-
+training794615332 = training[training['KIT_ID'] == 794615332 ]
+training794615332.plot(x='TS',y='USAGE',color='red',figsize=(15,2.5), linewidth=1, fontsize=10)
+#kit3409364152.plot(x='TS',y='AVG_SPEED_DW',color='red')#costante
+training794615332.plot(x='TS',y='VAR_CLASS',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)#costante
 
 
 ##############PLOT############################################################
@@ -243,12 +285,15 @@ training1 = training[training['VAR_CLASS'] == 1]
 training1['KIT_ID'].unique()# trovare gli unici KIT_ID che hanno avuto un disservizio di tipo 1
 
 kit3409364152 = training[training['KIT_ID'] == 3409364152]
-kit3409364152.plot(x='TS',y='USAGE',color='red',figsize=(15,2.5), linewidth=1, fontsize=10)
-#kit3409364152.plot(x='TS',y='AVG_SPEED_DW',color='red')#costante
+training[training['AVG_SPEED_DW'] == 85320]['KIT_ID'].unique()
 kit3409364152.plot(x='TS',y='VAR_CLASS',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)#costante
+kit3409364152.plot(x='TS',y='USAGE',color='red',figsize=(15,2.5), linewidth=1, fontsize=10)
+kit3409364152.plot(x='TS',y='NUM_CLI',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)#costante
+kit3409364152.plot(x='TS',y='AVG_SPEED_DW',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)#costante
+#kit3409364152.plot(x='TS',y='AVG_SPEED_DW',color='red')#costante
 plt.show()
 
-kit3409364152[kit3409364152['VAR_CLASS'] == 2]
+kit3409364152[kit3409364152['VAR_CLASS'] == 2]['AVG_SPEED_DW']
 kit3409364152[kit3409364152['VAR_CLASS'] == 1]
 kit3409364152[kit3409364152['VAR_CLASS'] == 0].tail(320)[['USAGE','KIT_ID','AVG_SPEED_DW','NUM_CLI']]
 
@@ -262,6 +307,7 @@ ax2 = ax1.twiny()
 fig.autofmt_xdate()
 kit1629361016.plot(x='TS',y='USAGE',color='red',figsize=(15,2.5), linewidth=1, fontsize=10)
 kit1629361016.plot(x='TS',y='VAR_CLASS',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)
+kit1629361016.plot(x='TS',y='NUM_CLI',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)
 plt.xlabel('Index Values')
 plt.ylabel('Elements in List Y')
 plt.show()
@@ -298,10 +344,14 @@ ax1.plot(kit1629361016['TS'],kit1629361016['VAR_CLASS'],'b-')
 ax2.plot(kit1629361016['TS'],kit1629361016['USAGE'],'r-')
 plt.show()
 
-kit1629361016[['USAGE', 'VAR_CLASS']][:10000].plot(x='TS',figsize=(15,2.5), linewidth=1, fontsize=10)
+#kit1629361016[['USAGE', 'VAR_CLASS']][:10000].plot(x='TS',figsize=(15,2.5), linewidth=1, fontsize=10)
+#kit1629361016.plot(x='TS',y='AVG_SPEED_DW',color='red',figsize=(20,10), linewidth=5, fontsize=5)#costante
+#kit1629361016.plot(x='TS',y='NUM_CLI',color='red',figsize=(20,10), linewidth=5, fontsize=5)#costante
+kit1629361016.plot(x='TS',y='USAGE',color='red',figsize=(15,2.5), linewidth=1, fontsize=10)
+kit1629361016.plot(x='TS',y='VAR_CLASS',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)
+kit1629361016.plot(x='TS',y='NUM_CLI',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)
 #plt.xlabel('TS', fontsize=10);
 #plt.ylabel('USAGE in bit/s', fontsize=10);
-kit1629361016.plot(x='TS',y='AVG_SPEED_DW',color='red',figsize=(20,10), linewidth=5, fontsize=5)#costante
 
 kit1629361016[kit1629361016['VAR_CLASS'] == 2]
 kit1629361016[kit1629361016['VAR_CLASS'] == 1]
@@ -313,7 +363,9 @@ print(Counter(kit1629361016['VAR_CLASS']))#({0: 8024, 2: 312, 1: 12})
 kit2487219358 = training[training['KIT_ID'] == 2487219358]
 #kit2487219358.plot(x='TS',y='AVG_SPEED_DW',color='red')#costante
 kit2487219358.plot(x='TS',y='USAGE',color='red',figsize=(15,2.5), linewidth=1, fontsize=10)
+kit2487219358.plot(x='TS',y='NUM_CLI',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)#costante
 kit2487219358.plot(x='TS',y='VAR_CLASS',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)#costante
+kit2487219358.plot(x='TS',y='AVG_SPEED_DW',color='blue',figsize=(15,2.5), linewidth=1, fontsize=10)#costante
 plt.show()
 
 kit2487219358[kit2487219358['VAR_CLASS'] == 2]
@@ -325,11 +377,50 @@ print(Counter(kit2487219358['VAR_CLASS']))#({0: 3423, 2: 20, 1: 12})
 
 
 
-
-
-
-
-
+#'''
+#Split train/test from any given data point.
+#:parameter
+#    :param ts: pandas Series
+#    :param test: num or str - test size (ex. 0.20) or index position
+#                 (ex. "yyyy-mm-dd", 1000)
+#:return
+#    ts_train, ts_test
+#'''
+#def split_train_test(ts, test=0.20, plot=True, figsize=(15,5)):
+#    ## define splitting point
+#    if type(test) is float:
+#        split = int(len(ts)*(1-test))
+#        perc = test
+#    elif type(test) is str:
+#        split = ts.reset_index()[ 
+#                      ts.reset_index().iloc[:,0]==test].index[0]
+#        perc = round(len(ts[split:])/len(ts), 2)
+#    else:
+#        split = test
+#        perc = round(len(ts[split:])/len(ts), 2)
+#    print("--- splitting at index: ", split, "|", 
+#          ts.index[split], "| test size:", perc, " ---")
+#    
+#    ## split ts
+#    ts_train = ts.head(split)
+#    ts_test = ts.tail(len(ts)-split)
+#    if plot is True:
+#        fig, ax = plt.subplots(nrows=1, ncols=2, sharex=False, 
+#                               sharey=True, figsize=figsize)
+#        ts_train.plot(ax=ax[0], grid=True, title="Train", 
+#                      color="black")
+#        ts_test.plot(ax=ax[1], grid=True, title="Test", 
+#                     color="black")
+#        ax[0].set(xlabel=None)
+#        ax[1].set(xlabel=None)
+#        plt.show()
+#        
+#    return ts_train, ts_test
+#
+#
+#kit2487219358_train , kit2487219358_test = split_train_test(kit2487219358[['TS','USAGE']])
+#
+#plot(kit2487219358[['TS','USAGE']], plot_ma=True, plot_intervals=True, window=w, figsize=(15,5))
 
 
 
@@ -342,3 +433,12 @@ print(Counter(kit2487219358['VAR_CLASS']))#({0: 3423, 2: 20, 1: 12})
 ########################################Parte relatica alla lettura del TEST.csv
 #test['TS'] = pd.to_numeric(test['TS'], downcast='float', errors='ignore')
 #test = pd.read_csv('test.csv', sep=';')       
+
+
+pulire il training dai kitid con 1 e 2(
+        allenare il model con il nuovo dataset, riallenarlo con il primo kitid1, kitid2,kitid3)
+pulire il training dai kitid con 0 e allenare il modello
+pulire il training dai kitid con 0 e allenare il modello, e riallenarlo con il training pulito da 1 e 2
+
+
+
